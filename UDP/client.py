0749094a395c3517e4acf98bd9c26b4937f8d527
JackKuo <kuohfjack@gmail.com>
2017-09-29 01:12:08 +0800

    add comment and change some variable name

--- UDP/client.py
+++ UDP/client.py
@@ -2,82 +2,72 @@ import datetime
 import time
 from threading import Thread
 import socket
-import psutil
 from os import system
 import sys
-
 
 class monocell(Thread):
 
     def __init__(self,name):
 
         Thread.__init__(self)
-
-        self.birthTime = datetime.datetime.now()
-
         self.name = name
         
-
     def run(self):
-
         heart(self.name)
 
 
-def speed_convert(data):
+def speed_convert(recv_size):    #for human-readable output
 
-    calc = data*8
-    if calc > 1024:
-        if calc/1024 > 1024:
-            return str(float(calc)/1024/1024) + " Mb"
+    calc = recv_size*8           # convert from byte to bit
+    if calc > 1024:              # if > 1Kb
+        if calc/1024 > 1024:     # if > 1Mb
+            return str(float(calc)/1024/1024) + " Mbps"
         else:
-            return  str(float(calc)/1024) + " Kb"
+            return  str(float(calc)/1024) + " Kbps"
     else:
-        return str(calc) + " bytes"
+        return str(calc) + " bps"
 
 
 def heart(name):
 
-    global data,avg_speed,count
+    global recv_size,speed_sum,count
     if name=="speed":
         while True:
-            time.sleep(1)
-            if data == 4:
+            time.sleep(1)                                       # pause for 1 second
+            if recv_size == 4:                                  # finish signal "done" size
                 break
-            if data > 0:
-                count  = count + 1
-            if count > 4 and count < 10:
-                avg_speed = avg_speed + data
-            print speed_convert(data)
-            data = 0
+            if recv_size > 0:
+                count  = count + 1                              # how many second after starting to record
+            if count > start_measure and count < stop_measure:  # record only in this period
+                speed_sum = speed_sum + recv_size
+            print speed_convert(recv_size)                      # print realtime speed statistics for debugging
+            recv_size = 0
 
     if name=="receive":
             while 1:
-                recv_data, addr = sock.recvfrom(2048)
-                #print recv_data
-                #print len(recv_data)
-                data = data + len(recv_data) + 42
-                if len(recv_data) == 4:
-                    data = 4
+                recv_recv_size, addr = sock.recvfrom(2048)
+                recv_size = recv_size + len(recv_recv_size) + 42
+                if len(recv_recv_size) == 4:
+                    recv_size = 4
                     break
             print "Client's upload test finish"
-            #print "###### download speed: " + speed_convert(avg_speed/(second-3)) + " ######"
-            print "###### download speed: " + speed_convert(avg_speed/5) + " ######"
+            print "###### download speed: " + speed_convert(speed_sum/period) + " ######"
 
     if name=="send":
         print "Uploading..."
-        t = time.time()
+        t = time.time()                                     # Start time
         while True:
-            sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-            if time.time() - t > second:
+            sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))        # send garbage
+            if time.time() - t > second:                    # if sending for N seconds
                 for i in range(5):
-                    sock.sendto("done", (UDP_IP, UDP_PORT))
+                    sock.sendto("done", (UDP_IP, UDP_PORT)) # send finish signal
                 time.sleep(1)
                 for i in range(5):
                     sock.sendto("done", (UDP_IP, UDP_PORT))
                 break
         print "Upload test finish"
-        recv_data, addr = sock.recvfrom(2048)
-        print "###### upload speed: " + recv_data + " ######"
+        recv_recv_size, addr = sock.recvfrom(2048)          # receive average speed statistic from receiver
+        print "###### upload speed: " + recv_recv_size + " ######"
         time.sleep(4)
         print "Waiting for download test..."
         try:
@@ -91,12 +81,15 @@ def heart(name):
             
 if __name__ == "__main__":
     second = 10
-    UDP_IP = "10.21.20.174"
-    UDP_PORT = 5005
-    count = 0
-    avg_speed = 0
-    MESSAGE = "Hello, World!" * 113
-    data = 0
+    UDP_IP = "10.21.20.174"                     # The default host
+    UDP_PORT = 5005                             # port used by the server
+    count = 0                                   # receive data for N seconds
+    speed_sum = 0                               # accumulate every one second speed avg
+    start_measure = 4                           # what time to accumulate speed avg to $speed_sum
+    stop_measure = 10                           # what time to stop accumulate speed avg
+    period = stop_measure - start_measure - 1   # the time from $start_measure to $stop_measure
+    MESSAGE = "Hello, World!" * 113             # payload
+    recv_size = 0
     if len(sys.argv) > 1:
         UDP_IP = sys.argv[1]
     if len(sys.argv) > 2:
@@ -109,10 +102,7 @@ if __name__ == "__main__":
                          socket.SOCK_DGRAM) # UDP
         except:
             print "connection refuse"
-    #    speed = monocell("speed")
         send = monocell("send")
-
-    #    speed.start()
         send.start()
     except:
         print "internal error!"
---
+++ UDP/server.py
@@ -0,0 +1,128 @@
+import datetime
+import time
+from threading import Thread
+import socket
+from os import system
+import sys
+
+
+class monocell(Thread):
+
+    def __init__(self,name):
+
+        Thread.__init__(self)
+        self.name = name
+
+    def run(self):
+
+        heart(self.name)
+
+
+def speed_convert(recv_size):                           #for human-readable output
+
+    calc = recv_size*8                                  # convert from byte to bit
+    if calc > 1024:                                     # if > 1Kb
+        if calc/1024 > 1024:                            # if > 1Mb
+            return str(float(calc)/1024/1024) + " Mbps"
+
+        else:
+            return  str(float(calc)/1024) + " Kbps"
+    else:
+        return str(calc) + " bps"
+
+
+def heart(name):
+
+    global recv_size,UDP_IP,Client_info,speed_sum,count
+    if name=="speed":
+        while True:
+            time.sleep(1)
+            if recv_size == 4:                          # finish signal "done" size
+                break
+            if recv_size > 0:
+                count  = count + 1                      # how many second after starting to record
+            if count > start_measure and count < stop_measure: # record only in this period
+                speed_sum = speed_sum + recv_size
+            print speed_convert(recv_size)              # print realtime speed statistics for debugging
+            recv_size = 0
+
+    if name=="send":
+        print "Uploading..."
+        t = time.time()                                 # Start time
+        while True:
+            #sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+            sock.sendto(MESSAGE, Client_info)           # send garbage
+            if time.time() - t > second:                # if sending for N seconds
+                for i in range(5):
+                    sock.sendto("done", Client_info)
+                time.sleep(1)
+                for i in range(5):
+                    sock.sendto("done", Client_info)
+                time.sleep(1)
+                for i in range(5):
+                    sock.sendto("done", Client_info)
+                break
+        print "Upload test finish"
+
+    if name=="receive":
+        print "Waiting for Upload test..."
+        count = 0
+        while 1:
+            recv_recv_size, addr = sock.recvfrom(2048)
+            #print len(recv_recv_size)
+            recv_size = recv_size + len(recv_recv_size) + 42
+            if len(recv_recv_size) == 4:
+                recv_size = 4
+                break
+        time.sleep(1)
+        print "Client's upload test finish"
+        print "Preparing for client's download"
+        Client_info = addr
+        #avg = speed_convert(speed_sum/(second-3))
+        avg = speed_convert(speed_sum/period)
+        print "Client's upload: " + avg
+        for i in range(100):                   # send average speed statistic from sender
+            sock.sendto(avg, Client_info)
+        time.sleep(1)
+        for i in range(50):
+            sock.sendto(avg, Client_info)
+        time.sleep(4)
+        print UDP_IP
+        try:
+        #    speed = monocell("speed")
+            send = monocell("send")
+
+        #    speed.start()
+            send.start()
+        except:
+            print "internal error!"
+            system("pause")    
+
+if __name__ == "__main__":
+    second = 10
+    UDP_IP = ""                                 # Symbolic name meaning all available interfaces
+    UDP_PORT = 5005                             # Arbitrary non-privileged port
+    Client_info = ""                            # to record client's information
+    recv_size = 0
+    speed_sum = 0
+    count = 0
+    start_measure = 4                           # what time to accumulate speed avg to $speed_sum
+    stop_measure = 10                           # what time to stop accumulate speed avg
+    period = stop_measure - start_measure - 1   # the time from $start_measure to $stop_measure
+    MESSAGE = "Hello, World!" * 113             # payload
+    if len(sys.argv) > 1:
+        UDP_PORT = int(sys.argv[1])
+    try:
+        sock = socket.socket(socket.AF_INET,    # Internet
+                         socket.SOCK_DGRAM)     # UDP
+        sock.bind((UDP_IP, UDP_PORT))
+        try:
+            speed = monocell("speed")
+            receive = monocell("receive")
+
+            speed.start()
+            receive.start()
+        except:
+            print "internal error!"
+    except:
+        print "socket error"
